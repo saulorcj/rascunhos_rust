6b96f3a44add73ece72b184787eb2ba9bf4dd4c1
Saulo Roberto <sauloroberto1000@hotmail.com>
2022-09-02 23:41:49 -0300

    variáveis finalizada

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,8 +1,26 @@
-const PONTOS_MAXIMOS: u32 = 100_000;
+/*const PONTOS_MAXIMOS: u32 = 100_000;
 
 fn main() {
     let mut x = 5;
     println!("O valor de x é: {}", x);
     x = 6;
     println!("O valor de x é: {}", x + PONTOS_MAXIMOS);
+}*/
+
+/*fn main() {
+    let x = 5;
+
+    let x = x + 1;
+
+    let x = x * 2;
+
+    println!("O valor de x é {}", x);
+}*/
+
+fn main() {
+    let espacos = "     ";
+
+    let espacos = espacos.len();
+
+    println!("São {} espaços!", espacos);
 }
