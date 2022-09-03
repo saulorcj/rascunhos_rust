0f0767f0a4ce6030eba2e2771df69e299c670e36
Saulo Roberto <sauloroberto1000@hotmail.com>
2022-09-03 01:33:24 -0300

    tipos de dados finalizado

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,13 +1,12 @@
-/*const PONTOS_MAXIMOS: u32 = 100_000;
+//const PONTOS_MAXIMOS: u32 = 100_000;
 
 fn main() {
+    /*
     let mut x = 5;
     println!("O valor de x é: {}", x);
     x = 6;
     println!("O valor de x é: {}", x + PONTOS_MAXIMOS);
-}*/
 
-/*fn main() {
     let x = 5;
 
     let x = x + 1;
@@ -15,12 +14,90 @@ fn main() {
     let x = x * 2;
 
     println!("O valor de x é {}", x);
-}*/
 
-fn main() {
     let espacos = "     ";
 
     let espacos = espacos.len();
 
     println!("São {} espaços!", espacos);
+    */
+
+    //let guess: u32 = "42".parse().expect("Não é um número!");
+
+    /*
+    let inteiro_sem_sinal: u8 = 255; // limite de 255
+
+    println!("{}", inteiro_sem_sinal);
+    
+
+    let x = 2.0; // f64
+
+    let y: f32 = 3.0; // f32
+    
+
+    // adição
+    let soma = 5 + 10;
+
+    // subtração
+    let diferenca = 95.5 - 4.3;
+
+    // multiplicação
+    let produto = 4 * 30;
+
+    // divisão
+    let quociente = 56.7 / 32.2;
+
+    // resto
+    let resto = 43 % 5;
+
+    let t = true;
+
+    let f: bool = false; // com tipo explícito
+    
+
+    let c = 'z';
+    let z = 'ℤ';
+    let heart_eyed_cat = '😻';
+    */
+    
+    // let tup: (i32, f64, u8) = (500, 6.4, 1);
+
+    /*
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+
+    let (x, y, z) = tup;
+
+    println!("O valor de y é: {}", y);
+    */
+
+    /*
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+
+    let quinhentos = x.0;
+
+    let seis_ponto_quatro = x.1;
+
+    let um = x.2;
+    */
+
+    // let a = [1, 2, 3, 4, 5];
+
+    // let meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Mario", "Junho", 
+    //              "Julho", "Setembro", "Outubro", "Novembro", "Dezembro"];
+    
+    /*
+    let a = [1, 2, 3, 4, 5];
+
+    let primeiro = a[0];
+    let segundo = a[1];
+    */
+
+    /*
+    let a = [1, 2, 3, 4, 5];
+    let indice = 10;
+
+    let elemento = a[indice];
+
+    println!("O valor do elemento é: {}", elemento)
+    */
 }
