ca846212426ebe9f96b798644d943ec893828703
Saulo Roberto <sauloroberto1000@hotmail.com>
2022-09-04 22:12:56 -0300

    começando a parte de funções

---
+++ funcoes/target/debug/deps/funcoes-a625d653a4b71183.d
@@ -0,0 +1,5 @@
+/home/saulo/Arquivos/rascunhos_rust/funcoes/target/debug/deps/funcoes-a625d653a4b71183: src/main.rs
+
+/home/saulo/Arquivos/rascunhos_rust/funcoes/target/debug/deps/funcoes-a625d653a4b71183.d: src/main.rs
+
+src/main.rs:
---
+++ funcoes/target/debug/funcoes.d
@@ -0,0 +1,1 @@
+/home/saulo/Arquivos/rascunhos_rust/funcoes/target/debug/funcoes: /home/saulo/Arquivos/rascunhos_rust/funcoes/src/main.rs
