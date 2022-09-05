c71dd488197e5d6fb74275457096d7ee1a64ff6a
Saulo Roberto <sauloroberto1000@hotmail.com>
2022-09-04 22:58:31 -0300

    v4

--- fluxos/src/main.rs
+++ fluxos/src/main.rs
@@ -1,13 +1,10 @@
 fn main() {
-    let numero = 6;
-
-    if numero % 4 == 0 {
-        println!("número é divisível por 4");
-    } else if numero % 3 == 0 {
-        println!("número é divisível por 3");
-    } else if numero % 2 == 0 {
-        println!("número é divisível por 2");
+    let condicao = true;
+    let numero = if condicao {
+        5
     } else {
-        println!("número não é divisível por 4, 3 ou 2");
-    }
+        6
+    };
+
+    println!("O valor do número é: {}", numero);
 }
