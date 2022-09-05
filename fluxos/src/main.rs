3625f1393db46460397f696075583edb14321785
Saulo Roberto <sauloroberto1000@hotmail.com>
2022-09-04 22:52:30 -0300

    v1

--- fluxos/src/main.rs
+++ fluxos/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let numero = 3;
+
+    if numero < 5 {
+        println!("condição era verdadeira!");
+    } else {
+        println!("condição era falsa");
+    }
 }
