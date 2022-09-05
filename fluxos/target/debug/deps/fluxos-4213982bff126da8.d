7585299a83be230aedaed841765dd026ad36add7
Saulo Roberto <sauloroberto1000@hotmail.com>
2022-09-04 22:49:25 -0300

    começando parte de fluxos

---
+++ fluxos/target/debug/deps/fluxos-4213982bff126da8.d
@@ -0,0 +1,5 @@
+/home/saulo/Arquivos/rascunhos_rust/fluxos/target/debug/deps/fluxos-4213982bff126da8: src/main.rs
+
+/home/saulo/Arquivos/rascunhos_rust/fluxos/target/debug/deps/fluxos-4213982bff126da8.d: src/main.rs
+
+src/main.rs:
---
+++ fluxos/target/debug/fluxos.d
@@ -0,0 +1,1 @@
+/home/saulo/Arquivos/rascunhos_rust/fluxos/target/debug/fluxos: /home/saulo/Arquivos/rascunhos_rust/fluxos/src/main.rs
